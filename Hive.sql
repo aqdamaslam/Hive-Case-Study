36cdf82a83873de23a13da4c34e40348fe596e21
Aqdam Aslam <56361347+aqdamaslam@users.noreply.github.com>
2025-01-17 15:05:13 +0530

    Update Hive.sql

--- Hive.sql
+++ Hive.sql
@@ -51,3 +51,26 @@ when Age between 46 and 60 then '46-60'
 else '61+'
 end;
 
+-- 4.	Count the number of records that have missing values in any field.
+
+select count(*) from car_insurance_calls
+where
+    Id is null
+    or Age is null
+    or Job is null
+    or Marital is null
+    or Education is null
+    or Default is null
+    or Balance is null
+    or HHInsurance is null
+    or CarLoan is null
+    or Communication is null
+    or LastContactDay is null
+    or LastContactMonth is null
+    or NoOfContacts is null
+    or DaysPassed is null
+    or PrevAttempts is null
+    or Outcome is null
+    or CallStart is null
+    or CallEnd is null
+    or CarInsurance is null;
