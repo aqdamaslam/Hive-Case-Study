78f1b9f8365ac23044cf4703adc613753566cce1
Aqdam Aslam <56361347+aqdamaslam@users.noreply.github.com>
2025-01-17 14:14:23 +0530

    Update Hive.sql

--- Hive.sql
+++ Hive.sql
@@ -1,1 +1,25 @@
 -- 1.	Load the data into a Hive table. Create an external table with the given schema and load the data into the table from a text file or HDFS path.
+
+CREATE EXTERNAL TABLE IF NOT EXISTS car_insurance_calls (
+    Id INT,
+    Age INT,
+    Job STRING,
+    Marital STRING,
+    Education STRING,
+    Default STRING,
+    Balance FLOAT,
+    HHInsurance STRING,
+    CarLoan STRING,
+    Communication STRING,
+    LastContactDay INT,
+    LastContactMonth STRING,
+    NoOfContacts INT,
+    DaysPassed INT,
+    PrevAttempts INT,
+    Outcome STRING,
+    CallStart STRING,
+    CallEnd STRING,
+    CarInsurance STRING
+)
+STORED AS TEXTFILE
+LOCATION 'hdfs://path/to/your/data';
