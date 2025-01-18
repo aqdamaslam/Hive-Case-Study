78fdc6669cfbd8886bfbc7485b59a8ff1814d316
Aqdam Aslam <56361347+aqdamaslam@users.noreply.github.com>
2025-01-18 20:14:40 +0530

    Update Hive.sql

--- Hive.sql
+++ Hive.sql
@@ -1,6 +1,6 @@
 -- 1.	Load the data into a Hive table. Create an external table with the given schema and load the data into the table from a text file or HDFS path.
 
-CREATE EXTERNAL TABLE IF NOT EXISTS car_insurance_calls (
+create external table if not exists car_insurance_calls (
     Id INT,
     Age INT,
     Job STRING,
@@ -180,3 +180,33 @@ from car_insurance_calls;
 
 
 -- 2.	Create a bucketed table on 'Age', bucketed into 4 groups (as per the age groups mentioned above). Load data from the original table into this bucketed table.
+
+create table if not exists car_insurance_calls (
+    Id INT,
+    Age INT,
+    Job STRING,
+    Marital STRING,
+    Education STRING,
+    `Default` STRING,
+    Balance FLOAT,
+    HHInsurance STRING,
+    CarLoan STRING,
+    Communication STRING,
+    LastContactDay INT,
+    LastContactMonth STRING,
+    NoOfContacts INT,
+    DaysPassed INT,
+    PrevAttempts INT,
+    Outcome STRING,
+    CallStart STRING,
+    CallEnd STRING,
+    CarInsurance STRING
+)
+clustered by(Age) into 4 buckets
+row format delimited
+fields terminated by ','
+stored as textfile;
+
+
+-- 
+
